9ac64c85429fae8672210428f309340fac5dfeb7
andreyVetlugin <andrey.vetlugin@yandex.ru>
2017-10-14 21:06:47 +0600

    with collision 1

--- NewTry.cpp
+++ NewTry.cpp
@@ -65,7 +65,7 @@ class Map
 		for (int j = 0; j < height; j++)
 			for (int i = 0; i < width; i++)
 			{
-				if (map[i + j*width] == 1)
+				if (map[i + j*width] == '1')
 				{
 					qwe[i + j*width].setTexture(texture);//тайл какойнить
 					qwe[i + j*width].setPosition(i*blockSize, j*blockSize);
@@ -83,25 +83,72 @@ public:
 	sf::Texture unitTexture;
 	sf::Image unitImage;
 	sf::Sprite unitSprite;
+	int pixelsSizeX = 40;
+	int pixelsSizeY = 40;
+	sf::FloatRect PhisicRect;
+	float currentFrame;
+	float Dx = 0, Dy=0;//set get
 	Unit(std::string pathForTexture)//?
 	{
 		unitImage.loadFromFile(pathForTexture);
 		unitTexture.loadFromImage(unitImage);
 		unitSprite.setTexture(unitTexture);
-		unitSprite.setPosition(0, 0);
+		unitSprite.setPosition(0,0);
+		PhisicRect=sf::FloatRect(0, 0, 40, 40);
 	}
+	void Step(float time,char* map) 
+	{	
 
+
+
+		//unitSprite.setPosition(unitSprite.getPosition().x + time*Dx,unitSprite.getPosition().y+Dy);
+		PhisicRect.left += time*Dx;
+		Collision(map);
+
+		PhisicRect.top += time*Dy;		
+		Collision(map);//time?;
+		unitSprite.setPosition(PhisicRect.left, PhisicRect.top);
+		Dx = 0; Dy = 0;
+	}
+	void Collision(char* map)
+	{
+
+		for (int j = PhisicRect.top / blockSize; j<(PhisicRect.top + PhisicRect.height) / blockSize; j++)
+			for (int i = PhisicRect.left / blockSize; i<(PhisicRect.left + PhisicRect.width) / blockSize; i++)
+			{
+				if (map[i+width*j] == '0') 
+				{
+					if (Dy>0)
+					{
+						PhisicRect.top = j * blockSize - PhisicRect.height;  Dy = 0;
+					}
+					else if (Dy<0)
+					{
+						PhisicRect.top = j*blockSize+ blockSize;   Dy = 0;
+					}
+					else if (Dx>0 )
+					{
+						PhisicRect.left = i * blockSize-PhisicRect.width;Dx=0;
+					}
+					else if (Dx<0 )
+					{
+						PhisicRect.left = i * blockSize+blockSize;Dx=0;
+					}
+				}				
+			}
+
+	}
 
 };
 void main() 
 {	//////////test sozdat' mapy texturkoy dlya oobrabotki colizii
 
-	sf::Sprite test[width*height * 4];
+	//sf::Sprite test[width*height * 4];
 
 	//////
 	Map *zap= new Map();
 
-	zap->test(test);
+	//zap->test(test);
 
 	float speed = 0.05;
 	int delay =300;
@@ -125,31 +172,30 @@ void main()
 		}
 		
 		if (Keyboard::isKeyPressed(Keyboard::Right))
-			unit.unitSprite.setPosition(unit.unitSprite.getPosition().x + time*speed, unit.unitSprite.getPosition().y);
+			unit.Dx = speed;
 		if (Keyboard::isKeyPressed(Keyboard::Left))
-			unit.unitSprite.setPosition(unit.unitSprite.getPosition().x - time*speed, unit.unitSprite.getPosition().y);
+			unit.Dx = -speed;
 		if (Keyboard::isKeyPressed(Keyboard::Up))
-			unit.unitSprite.setPosition(unit.unitSprite.getPosition().x , unit.unitSprite.getPosition().y-time*speed);
+			unit.Dy = -speed;
 		if (Keyboard::isKeyPressed(Keyboard::Down))
-			unit.unitSprite.setPosition(unit.unitSprite.getPosition().x , unit.unitSprite.getPosition().y+time*speed);
+			unit.Dy = speed;
+		unit.Step(time,zap->map);
 		window.clear();
-		std::cout <<unit.unitSprite.getPosition().x<<"     ";
+		//std::cout << ;
 		
 		
 		for (int j = 0; j < height; j++)
 		{
 			for (int i = 0; i < width; i++)
-				window.draw(test[i + j*width]);
-				/*if ( zap->GetElement(i + j*width)== '1')
+				/*window.draw(test[i + j*width]);*/
+				if ( zap->GetElement(i + j*width)== '1')
 				{
 					static sf::RectangleShape circle(sf::Vector2f(blockSize, blockSize));
 					circle.setPosition(sf::Vector2f(i*blockSize, j*blockSize));
 
 
-
-
 					window.draw(circle);
-				}*/
+				}
 		}
 		window.draw(unit.unitSprite);
 		window.display();
