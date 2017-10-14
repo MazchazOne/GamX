a6f661af5b8e95f413c25d27bebb38b657b8eb32
andreyVetlugin <andrey.vetlugin@yandex.ru>
2017-10-14 23:34:18 +0600

    control with mouse 1

--- NewTry.cpp
+++ NewTry.cpp
@@ -6,6 +6,53 @@ using namespace sf;
 const int width = 68, height = 35,blockSize=20;//карта
 const char blockChar = '1'; char nothingChar = '0';
 
+class Path
+{public:
+	static void ChangedCoordForNextStep(float*x, float* y, sf::FloatRect currentCoordIn, sf::FloatRect targetCoord)
+	{
+		sf::FloatRect currentCoord(currentCoordIn);
+		currentCoord.left += currentCoord.width/2;
+		currentCoord.top += currentCoord.height / 2;
+		currentCoord.height = 4;
+		currentCoord.width = 4;
+		if (currentCoord.intersects(targetCoord))
+		{
+			*x = 0;
+			*y = 0;
+			return;
+		}
+
+		int Dx = abs(currentCoord.left - targetCoord.left);
+			int Dy = abs(currentCoord.top - targetCoord.top);
+			if (Dx > Dy)
+			{
+				if (currentCoord.left < targetCoord.left)
+					*x += 0.05;
+				else if (currentCoord.left > targetCoord.left)
+					*x -= 0.05;
+				else *x = 0;
+				if (currentCoord.top < targetCoord.top)
+					*y += 0.05;
+				else if (currentCoord.top > targetCoord.top)
+					*y -= 0.05;
+				else *y = 0;
+			}
+			else
+			{
+				if (currentCoord.top < targetCoord.top)
+					*y += 0.05;
+				else if (currentCoord.top > targetCoord.top)
+					*y -= 0.05;
+				else *y = 0;
+				if (!(currentCoord.intersects(targetCoord)) && (currentCoord.left < targetCoord.left))
+					*x += 0.05;
+				else if (currentCoord.left > targetCoord.left)
+					*x -= 0.05;	
+				else *x = 0;
+			}
+			
+	}
+};
 class Map 
 {	public:
 	char map[width*height * 4];
@@ -77,7 +124,6 @@ class Map
 	/*bool IsBlock()
 	{}*/
 };
-
 class Unit
 {
 public:	
@@ -86,67 +132,82 @@ public:
 	sf::Sprite unitSprite;
 	int pixelsSizeX = 40;
 	int pixelsSizeY = 40;
-	sf::FloatRect PhisicRect;
+	sf::FloatRect TargetPositionPoint;
+	bool FolowingTarget = false;
+	sf::FloatRect ActualPositionRect;
 	float currentFrame;
-	float Dx = 0, Dy=0;//set get
+	float Dx = 0, Dy=0;//set get 
+
 	Unit(std::string pathForTexture)//?
 	{
 		unitImage.loadFromFile(pathForTexture);
 		unitTexture.loadFromImage(unitImage);
 		unitSprite.setTexture(unitTexture);
 		unitSprite.setPosition(0,0);
-		PhisicRect=sf::FloatRect(0, 0, 40, 40);
+		ActualPositionRect=sf::FloatRect(0, 0, 40, 40);
+		FolowingTarget = false;
+	}
+	void SetTarget(int x,int y) 
+	{
+		TargetPositionPoint = sf::FloatRect(x, y, 1, 1);//zadat' netochnost'
+		FolowingTarget = true;
 	}
 	void Step(float time,char* map) 
-	{	
-		//unitSprite.setPosition(unitSprite.getPosition().x + time*Dx,unitSprite.getPosition().y+Dy);		
-		PhisicRect.top += time*Dy;
-		CollisionOnY(map);		
-		PhisicRect.left += time*Dx;		
-		CollisionOnX(map);//time?;
-		unitSprite.setPosition(PhisicRect.left, PhisicRect.top);
-		Dx = 0; Dy = 0;
+	{
+		Path::ChangedCoordForNextStep(&Dx, &Dy, ActualPositionRect, TargetPositionPoint);
+		//unitSprite.setPosition(unitSprite.getPosition().x + time*Dx,unitSprite.getPosition().y+Dy);	
+		if (ActualPositionRect.intersects(TargetPositionPoint))
+			FolowingTarget = false;
+		if (FolowingTarget == true)
+		{
+			ActualPositionRect.top += time*Dy;
+			CollisionOnY(map);
+			ActualPositionRect.left += time*Dx;
+			CollisionOnX(map);//time?;
+			unitSprite.setPosition(ActualPositionRect.left, ActualPositionRect.top);
+			Dx = 0; Dy = 0;			
+		}
 	}
 	void CollisionOnY(char* map)
 	{
 
-		for (int j = PhisicRect.top / blockSize; j<(PhisicRect.top + PhisicRect.height) / blockSize; j++)
-			for (int i = PhisicRect.left / blockSize; i<(PhisicRect.left + PhisicRect.width) / blockSize; i++)
+		for (int j = ActualPositionRect.top / blockSize; j<(ActualPositionRect.top + ActualPositionRect.height) / blockSize; j++)
+			for (int i = ActualPositionRect.left / blockSize; i<(ActualPositionRect.left + ActualPositionRect.width) / blockSize; i++)
 			{
 				if (map[i+width*j] == blockChar)
 				{
 					if (Dy>0)
 					{
-						PhisicRect.top = j * blockSize - PhisicRect.height;  Dy = 0;
+						ActualPositionRect.top = j * blockSize - ActualPositionRect.height;  Dy = 0;
 					}
 					else if (Dy<0)
 					{
-						PhisicRect.top = j*blockSize+ blockSize;   Dy = 0;
+						ActualPositionRect.top = j*blockSize+ blockSize;   Dy = 0;
 					}					
 				}				
 			}
 	}
-
 	void CollisionOnX(char* map)
 	{
 
-		for (int j = PhisicRect.top / blockSize; j<(PhisicRect.top + PhisicRect.height) / blockSize; j++)
-			for (int i = PhisicRect.left / blockSize; i<(PhisicRect.left + PhisicRect.width) / blockSize; i++)
+		for (int j = ActualPositionRect.top / blockSize; j<(ActualPositionRect.top + ActualPositionRect.height) / blockSize; j++)
+			for (int i = ActualPositionRect.left / blockSize; i<(ActualPositionRect.left + ActualPositionRect.width) / blockSize; i++)
 			{
 				if (map[i + width*j] == blockChar)
 				{				
 					if (Dx>0)
 					{
-						PhisicRect.left = i * blockSize - PhisicRect.width; Dx = 0;
+						ActualPositionRect.left = i * blockSize - ActualPositionRect.width; Dx = 0;
 					}
 					else if (Dx<0)
 					{
-						PhisicRect.left = i * blockSize + blockSize; Dx = 0;
+						ActualPositionRect.left = i * blockSize + blockSize; Dx = 0;
 					}
 				}
 			}
 	}
 };
+
 void main() 
 {	//////////test sozdat' mapy texturkoy dlya oobrabotki colizii
 
@@ -177,15 +238,25 @@ void main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		if (event.type == sf::Event::MouseButtonPressed&&event.mouseButton.button == sf::Mouse::Right/*&&timerForLeftMouseButton<=0*/)
+		{
+			int x = sf::Mouse::getPosition().x - window.getPosition().x - 7;
+			int y = sf::Mouse::getPosition().y - window.getPosition().y - 31;
+			unit.SetTarget(x, y);
+			/*std::cout << sf::Mouse::getPosition().x-window.getPosition().x-7<<"   "<< sf::Mouse::getPosition().y-window.getPosition().y-31<<endl;
+			cout << x / blockSize + width*y / blockSize<<"="<<x/blockSize<< width*(y / blockSize);*/
+			
+			//timerForLeftMouseButton = 20;
+		}
 		
-		if (Keyboard::isKeyPressed(Keyboard::Right))
+		/*if (Keyboard::isKeyPressed(Keyboard::Right))
 			unit.Dx = speed;
 		if (Keyboard::isKeyPressed(Keyboard::Left))
 			unit.Dx = -speed;
 		if (Keyboard::isKeyPressed(Keyboard::Up))
 			unit.Dy = -speed;
 		if (Keyboard::isKeyPressed(Keyboard::Down))
-			unit.Dy = speed;
+			unit.Dy = speed;*/
 		unit.Step(time,zap->map);
 		window.clear();
 		//std::cout << ;
@@ -205,6 +276,13 @@ void main()
 				}
 		}
 		window.draw(unit.unitSprite);
+		/*static sf::RectangleShape circle(sf::Vector2f(40, 40));
+		circle.setPosition(sf::Vector2f(unit.ActualPositionRect.left, unit.ActualPositionRect.top));
+		window.draw(circle);*/
+		static sf::RectangleShape circle1(sf::Vector2f(2, 2));
+		circle1.setPosition(sf::Vector2f(unit.TargetPositionPoint.left, unit.TargetPositionPoint.top));
+		circle1.setFillColor(sf::Color::Red);
+		window.draw(circle1);
 		window.display();
 	}
 
